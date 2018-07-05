b5ec9faa5ab1946870ede9901e77a5d53eb6517f
songtm <565038419@qq.com>
2018-07-05 13:12:13 +0800

    copy protolexer.g4

--- src/main/gen/io/protostuff/jetbrains/plugin/ProtoLexer.java
+++ src/main/gen/io/protostuff/jetbrains/plugin/ProtoLexer.java
@@ -1,4 +1,5 @@
 // Generated from /Users/songtianming/Documents/dev/proj/protobuf-jetbrains-plugin/src/main/ProtoLexer.g4 by ANTLR 4.7
+package io.protostuff.jetbrains.plugin;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.Token;
