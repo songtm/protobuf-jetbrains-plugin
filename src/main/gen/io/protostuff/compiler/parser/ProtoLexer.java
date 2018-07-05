f252634e6c866c239545319087069ce5ab83981b
songtm <565038419@qq.com>
2018-07-05 13:34:44 +0800

    copy protolexer.g4 overwrite jar class

--- src/main/gen/io/protostuff/compiler/parser/ProtoLexer.java
+++ src/main/gen/io/protostuff/compiler/parser/ProtoLexer.java
@@ -1,5 +1,5 @@
 // Generated from /Users/songtianming/Documents/dev/proj/protobuf-jetbrains-plugin/src/main/ProtoLexer.g4 by ANTLR 4.7
-package io.protostuff.jetbrains.plugin;
+package io.protostuff.compiler.parser;
 import org.antlr.v4.runtime.Lexer;
 import org.antlr.v4.runtime.CharStream;
 import org.antlr.v4.runtime.Token;
